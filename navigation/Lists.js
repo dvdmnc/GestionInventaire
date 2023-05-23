942dc6f9ce086043c9fc262bf0919bb9e3a3379c
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2023-05-23 15:52:42 -0500

    Update Lists.js

--- navigation/Lists.js
+++ navigation/Lists.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {View, Text, SafeAreaView, ScrollView, StyleSheet, Image} from 'react-native';
-import { color } from 'react-native-reanimated';
+import BoxItemCategories from './BoxItemCategories';
+import {ListProduits, ListMateriel, ListAccessoires, 
+    ListNonSterile, ListSterile, ListsousFecale, ListDrainage, 
+    ListStomie, ListJusdeFruits, ListEauGelifiee, ListPoudreHyperProtidique,
+ListBoissonLactee, ListCreme, ListConcentreProteine, ListBoissonLacteeSpe} from './Sublists';
 
 const ListHygiene = () => {
     return(
@@ -57,28 +62,213 @@ const ListSoins = () => {
 };
 
 const ListPerfusions = () => {
-    return('');};
+    const [list, setList] = useState('');
+
+    const produitsclick = () => setList(<ListProduits/>);
+    const materielclick = () => setList(<ListMateriel/>);
+    const accessoiresclick = () => setList(<ListAccessoires/>);
+
+    return(
+        <View>
+            <Text style={styles.titlesubCategories}>Sous-Catégories</Text>
+            <View style= {styles.view}>
+                <BoxItemCategories
+                        image= {<Image source={require('./screens/Images/saline.png')} style={styles.Icons}/>}
+                        color="rgba(65,105,225,0.5)"
+                        text="Produits"
+                        onPress= {produitsclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/catheter.png')} style={styles.Icons}/>}
+                color="	rgba(255,0,0,0.5)"
+                text="Matériel"
+                onPress={materielclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/needle.png')} style={styles.Icons}/>}
+                color="rgba(102, 0, 204, 0.5)"
+                text="Accessoires CIP/PICC"
+                onPress={accessoiresclick}
+                />
+            </View>
+            {list}
+        </View>
+    );};
 
 const ListUrinaires = () => {
-    return('');};
+    const [list, setList] = useState('');
+
+    const nonsterileclick = () => setList(<ListNonSterile/>);
+    const sterileclick = () => setList(<ListSterile/>);
+
+    return(
+        <View>
+            <Text style={styles.titlesubCategories}>Sous-Catégories</Text>
+            <View style= {styles.view}>
+                <BoxItemCategories
+                        image= {<Image source={require('./screens/Images/warning.png')} style={styles.Icons}/>}
+                        color="rgba(255,192,58,0.5)"
+                        text="Non Stérile"
+                        onPress= {nonsterileclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/eliminate.png')} style={styles.Icons}/>}
+                color="	rgba(225,210,184,0.5)"
+                text="Stérile"
+                onPress={sterileclick}
+                />
+            </View>
+            {list}
+        </View>
+    );};
 
 const ListFecales = () => {
-    return('');};
+    const [list, setList] = useState('');
+
+    const sousfecaleclick = () => setList(<ListsousFecale/>);
+    const drainageclick = () => setList(<ListDrainage/>);
+    const stomieclick = () => setList(<ListStomie/>);
+
+    return(
+        <View>
+            <Text style={styles.titlesubCategories}>Sous-Catégories</Text>
+            <View style= {styles.view}>
+                <BoxItemCategories
+                        image= {<Image source={require('./screens/Images/prostate-exam.png')} style={styles.Icons}/>}
+                        color="rgb(255, 255, 255)"
+                        text="Fecale"
+                        onPress= {sousfecaleclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/digestion.png')} style={styles.Icons}/>}
+                color="	rgba(0,255,0,0.5)"
+                text="Drainage"
+                onPress={drainageclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/colon-cancer.png')} style={styles.Icons}/>}
+                color="rgba(255, 192, 203, 0.5)"
+                text="Stomie"
+                onPress={stomieclick}
+                />
+            </View>
+            {list}
+        </View>
+    );};
 
 const ListRespiratoire = () => {
-    return('');};
+    return(
+        <View>
+            <Text style= {styles.text}>Lunette O²</Text>
+            <Text style= {styles.text}>Masque aérosol</Text>
+            <Text style= {styles.text}>Gel Lubrifiant K.Y</Text>
+        </View>
+    );};
 
 const ListGlycemie = () => {
-    return('');};
+    return(
+    <View>
+        <Text style= {styles.text}>Lecteur de Glycémie</Text>
+        <Text style= {styles.text}>Bandelette Accu-Check guide B50</Text>
+        <Text style= {styles.text}>Autopiqueur Unistik S30</Text>
+        <Text style= {styles.text}>Aiguille de sécurité 4 mm S30</Text>
+        <Text style= {styles.text}>Aiguille de sécurité 8 mm S30</Text>
+    </View>
+    );};
 
 const ListNutrition = () => {
-    return('');};
+    return(
+    <View>
+            <Text style= {styles.text}>POCHES D'ALIMENTATION ENTERALES Fresub</Text>
+            <Text style= {styles.text}>POCHES D'ALIMENTATION ENTERALES Fresubin 2 Kcal HP Fibre 500ml</Text>
+            <Text style= {styles.text}>Seringue ENFit 50ml</Text>
+            <Text style= {styles.text}>Adaptateur Godet ENFit NCE100A</Text>
+            <Text style= {styles.text}>Adaptateur Fosset ENFit NCE1058A</Text>
+            <Text style= {styles.text}>Seringue de gavage 50ml</Text>
+            <Text style= {styles.text}>Fosset 478.00</Text>
+            <Text style= {styles.text}>SNG SDFT08</Text>
+            <Text style= {styles.text}>SNG SDFT10</Text>
+            <Text style= {styles.text}>SNG SDFT12</Text>
+            <Text style= {styles.text}>SNG SDFT14</Text>
+    </View>
+    );};
 
 const ListAspiration = () => {
-    return('');};
+    return(  
+    <View>
+        <Text style= {styles.text}>Sonde d'aspiration boite de 50 Noire CH10</Text>
+        <Text style= {styles.text}>Sonde d'aspiration boite de 50 Blanche CH12</Text>
+        <Text style= {styles.text}>Sonde d'aspiration boite de 50 Verte CH14</Text>
+        <Text style= {styles.text}>Sonde d'aspiration boite de 50 Orange CH16</Text>
+        <Text style= {styles.text}>Sonde d'aspiration boite de 50 Rouge CH18</Text>
+        <Text style= {styles.text}>Stop vide Uni'valve</Text>
+        <Text style= {styles.text}>Poly'vac (Bouchon)</Text>
+        <Text style= {styles.text}>Eau stérile versable 1000ml</Text>
+        <Text style= {styles.text}>Eau PPI 20ml</Text>
+    </View>
+    );};
 
 const ListComplements = () => {
-    return('');};
+    const [list, setList] = useState('');
+
+    const jusdefruitsclick = () => setList(<ListJusdeFruits/>);
+    const eaugelifieeclick = () => setList(<ListEauGelifiee/>);
+    const poudrehyperprotidiqueclick = () => setList(<ListPoudreHyperProtidique/>);
+    const boissonlacteeclick = () => setList(<ListBoissonLactee/>);
+    const cremeclick = () => setList(<ListCreme/>);
+    const concentreproteineclick = () => setList(<ListConcentreProteine/>);
+    const boissonlacteespelick = () => setList(<ListBoissonLacteeSpe/>);
+
+    return(
+        <View>
+            <Text style={styles.titlesubCategories}>Sous-Catégories</Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} style= {styles.scrollViewCategories}>
+                <BoxItemCategories
+                        image= {<Image source={require('./screens/Images/juice.png')} style={styles.Icons}/>}
+                        color="rgba(128, 0, 32, 0.5)"
+                        text="Jus de Fruits"
+                        onPress= {jusdefruitsclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/jelly.png')} style={styles.Icons}/>}
+                color="	rgb(255,255,255)"
+                text="Eau Gélifiée"
+                onPress={eaugelifieeclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/measuring-cup.png')} style={styles.Icons}/>}
+                color="rgba(0, 150, 255, 0.5)"
+                text="Poudre Hyper Protidique"
+                onPress={poudrehyperprotidiqueclick}
+                />
+                <BoxItemCategories
+                        image= {<Image source={require('./screens/Images/milkdrink.png')} style={styles.Icons}/>}
+                        color="rgb(128, 0, 0)"
+                        text="Boisson Lactée"
+                        onPress= {boissonlacteeclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/cream.png')} style={styles.Icons}/>}
+                color="rgb(240,234,214)"
+                text="Crème"
+                onPress={cremeclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/protein.png')} style={styles.Icons}/>}
+                color="rgba(0, 255, 0, 0.5)"
+                text="Concentré Protéines"
+                onPress={concentreproteineclick}
+                />
+                <BoxItemCategories
+                image= {<Image source={require('./screens/Images/warning.png')} style={styles.Icons}/>}
+                color="rgba(255, 234, 0, 0.5)"
+                text="Boisson Lactée Spécifique"
+                onPress={boissonlacteespelick}
+                />
+            </ScrollView>
+            {list}
+        </View>
+    );};
 
 const ListTpn = () => {
     return('');};
@@ -93,14 +283,44 @@ export {ListHygiene, ListSoins, ListPerfusions, ListUrinaires,
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        borderTopWidth: 3,
         marginTop: 5,
-        borderTopColor: '#45b3e0'
     },
     text: {
         paddingVertical : 5,
         borderBottomWidth: 1,
         borderBottomColor: '#45b3e0',
         color: '#45b3e0'
-    }
+    },
+    scrollViewCategories: {
+      paddingLeft: 20,
+    },
+    Icons: {
+      height: 60,
+      width: 60,
+    },
+    view:
+    {
+        flex: 1,
+        flexDirection: 'row',
+        paddingTop: 5,
+        borderTopWidth: 3,
+        borderTopColor: '#45b3e0',
+        paddingHorizontal: 10,
+        justifyContent: 'space-evenly'
+    },
+  titlesubCategories: {
+    fontSize: 18,
+    color: '#45b3e0',
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
+  },
+  scrollViewCategories: {
+    paddingLeft: 20,
+    borderTopWidth: 3,
+    borderTopColor: '#45b3e0',
+    paddingTop: 5,
+  }
 });
