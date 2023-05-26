bac1fac7149d7da38361d151af22048cbc820f23
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2023-05-26 15:23:10 -0500

    Add files via upload

---
+++ navigation/Elements.js
@@ -0,0 +1,524 @@
+export const elements= [
+    {
+      "id": "1",
+      "name": "Blouse",
+      "category": "Hygiène"
+    },
+    {
+      "id": "2",
+      "name": "Tablier Plastique",
+      "category": "Hygiène"
+    },
+    {
+      "id": "3",
+      "name": "Gants non stérile",
+      "category": "Hygiène"
+    },
+    {
+      "id": "4",
+      "name": "Masque",
+      "category": "Hygiène"
+    },
+    {
+      "id": "5",
+      "name": "Charlotte",
+      "category": "Hygiène"
+    },
+    {
+      "id": "6",
+      "name": "Gants Stériles",
+      "category": "Hygiène"
+    },
+    {
+      "id": "7",
+      "name": "Savon",
+      "category": "Hygiène"
+    },
+    {
+      "id": "8",
+      "name": "Aniosgel Pompe",
+      "category": "Hygiène"
+    },
+    {
+      "id": "9",
+      "name": "Anios Surfa'safe",
+      "category": "Hygiène"
+    },
+    {
+      "id": "10",
+      "name": "Salvanios",
+      "category": "Hygiène"
+    },
+    {
+      "id": "11",
+      "name": "Gants de toilette",
+      "category": "Hygiène"
+    },
+    {
+      "id": "12",
+      "name": "Batonnets Soin de bouche",
+      "category": "Hygiène"
+    },
+    {
+      "id": "13",
+      "name": "Comprimés soin de bouche",
+      "category": "Hygiène"
+    },
+    {
+      "id": "14",
+      "name": "Abaisse Langue",
+      "category": "Hygiène"
+    },
+    {
+      "id": "15",
+      "name": "Crachoir",
+      "category": "Hygiène"
+    },
+    {
+      "id": "16",
+      "name": "Haricot",
+      "category": "Hygiène"
+    },
+    {
+      "id": "17",
+      "name": "Collecteur objets piquants",
+      "category": "Hygiène"
+    },
+    {
+      "id": "18",
+      "name": "Sac DASRI",
+      "category": "Hygiène"
+    },
+    {
+      "id": "19",
+      "name": "Sac Poubelle",
+      "category": "Hygiène"
+    },
+    {
+      "id": "20",
+      "name": "Alese",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "21",
+      "name": "Set à Pansements",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "22",
+      "name": "Compresse 7.5*7.5 B50",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "23",
+      "name": "Compresse 10*10 B50",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "24",
+      "name": "Ciseaux",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "25",
+      "name": "Coupe Fils",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "26",
+      "name": "Curette",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "27",
+      "name": "Stylet",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "28",
+      "name": "Bande Nylex",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "29",
+      "name": "Bande Crêpe (Velpeau)",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "30",
+      "name": "Sparadrap Microporeux",
+      "category": "Matériel de soins"
+    },
+  
+    {
+      "id": "31",
+      "name": "Champ Stérile",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "32",
+      "name": "Ôte Agrafes",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "33",
+      "name": "Protecteur cutané Brava Max",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "34",
+      "name": "Spray Brava",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "35",
+      "name": "Spray Adapt",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "36",
+      "name": "Sanyrene Pomp",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "37",
+      "name": "Conveen Portact cr",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "38",
+      "name": "Verre Canard",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "39",
+      "name": "Pilulier",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "40",
+      "name": "Ecrase comprimés",
+      "category": "Matériel de soins"
+    },
+    {
+      "id": "41",
+      "name": "Dosette",
+      "category": "Perfusions - Produits"
+    },
+    {
+      "id": "42",
+      "name": "Spray Irriclens",
+      "category": "Perfusions - Produits"
+    },
+    {
+      "id": "43",
+      "name": "Poche 50ml",
+      "category": "Perfusions - Produits"
+    },
+    {
+        "id": "44",
+        "name": "Poche 100ml",
+        "category": "Perfusions - Produits"
+    },
+    {
+    "id": "45",
+    "name": "VESIRIG Instil Vesicale 250ml",
+    "category": "Perfusions - Produits"
+    },
+    {
+    "id": "46",
+    "name": "Perfuseur",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "47",
+    "name": "DOSIFLOW",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "48",
+    "name": "Prolongateur",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "49",
+    "name": "Rampe de 4 robinets 3 voies",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "50",
+    "name": "Robinet 3 voies",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "51",
+    "name": "Robinet 3 voies + prolongateur",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "52",
+    "name": "Caresite valve positive",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "53",
+    "name": "Bouchon Stopper",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "54",
+    "name": "Tubulure Sapphire",
+    "category": "Perfusions - Matériel"
+    },
+    {
+        "id": "55",
+        "name": "Prolongateur 2 voies",
+        "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "56",
+    "name": "Poche vide Sapphire 100ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "57",
+    "name": "Poche vide Sapphire 250ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "58",
+    "name": "Catheter ailette sécurisé 22G bleu",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "59",
+    "name": "Catheter sécurisé 20G rose",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "60",
+    "name": "Catheter sécurisé 22G bleu",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "61",
+    "name": "Catheter sécurisé 24G jaune",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "62",
+    "name": "Aiguille Trocart 18G rose",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "63",
+    "name": "Aiguille IM 22G noire",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "64",
+    "name": "Aiguille SC 25G orange",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "65",
+    "name": "Seringue à Insuline 1ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "66",
+    "name": "Seringue 2ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "67",
+    "name": "Seringue 5ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "68",
+    "name": "Seringue 10ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "69",
+    "name": "Seringue 20ml",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "70",
+    "name": "Seringue 20ml luer lock",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "71",
+    "name": "Seringue 50ml luer lock",
+    "category": "Perfusions - Matériel"
+    },
+
+    {
+    "id": "72",
+    "name": "Set de transfert",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "73",
+    "name": "Pst adhésif Tegaderm I.V. 7x8.5cm",
+    "category": "Perfusions - Matériel"
+    },
+    {
+    "id": "74",
+    "name": "Aiguille de Huber 19G - 15mm",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "75",
+    "name": "Aiguille de Huber 19G - 20mm",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "76",
+    "name": "Aiguille de Huber 19G - 25mm",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "77",
+    "name": "Aiguille de Huber 22G - 15mm",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "78",
+    "name": "Aiguille de Huber 22G - 20mm",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "79",
+    "name": "Pst piccline Grip-Lok Universel",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "80",
+    "name": "Set pose perfusion Piccline + Seringue pre-remplie de Nacl 0.9% 10ml",
+    "category": "Perfusions - Accessoires CIP/PICC"
+    },
+    {
+    "id": "81",
+    "name": "Pot à diurèse",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "82",
+    "name": "Pistolet",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "83",
+    "name": "Bassin",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "84",
+    "name": "Etui pénien Coloplast Conveen Optima Ø21",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+        "id": "85",
+        "name": "Etui pénien Coloplast Conveen Optima Ø25",
+        "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "86",
+    "name": "Etui pénien Coloplast Conveen Optima Ø30",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "87",
+    "name": "Etui pénien Coloplast Conveen Optima Ø35",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "88",
+    "name": "Etui pénien Coloplast Conveen Optima Ø40",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "89",
+    "name": "Etui pénien Braun Urimed Vision Ø25",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "90",
+    "name": "Etui pénien Braun Urimed Vision Ø29",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "91",
+    "name": "Etui pénien Braun Urimed Vision Ø32",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "92",
+    "name": "Poche à urine non stérile 1.5L",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "93",
+    "name": "Poche à urine non stérile 2.0L",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "94",
+    "name": "Support de poche à urine",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "95",
+    "name": "Filet de poche (B2)",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+        "id": "96",
+        "name": "Sonde auto-sondage Femme",
+        "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "97",
+    "name": "Sonde auto-sondage homme",
+    "category": "Soins Urinaires - Non Stérile"
+    },
+    {
+    "id": "98",
+    "name": "Set de sondage urinaire",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "99",
+    "name": "Sonde vésicale verte CH14",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "100",
+    "name": "Sonde vésicale orange CH16",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "101",
+    "name": "Sonde vésicale rouge CH18",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "102",
+    "name": "Sonde vésicale jaune CH20",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "103",
+    "name": "Sonde vésicale violette CH22",
+    "category": "Soins Urinaires - Stérile"
+    },
+    {
+    "id": "104",
+    "name": "Poche à urine stérile 2.0L",
+    "category": "Soins Urinaires - Stérile"
+    }
+   ];
