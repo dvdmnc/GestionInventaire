f6ca7113e682041213d325fdd96cb099b5e921fe
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2023-06-09 16:04:04 -0500

    Update Counter.js

--- navigation/screens/Counter.js
+++ navigation/screens/Counter.js
@@ -1,35 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button} from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, Text, Button, StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Counter = React.memo(({ element }) => {
+  const [quantity, setQuantity] = useState(0);
+  const navigation = useNavigation();
+
+  const handleIncrement = useCallback(() => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  }, []);
+
+  const handleDecrement = useCallback(() => {
+    if (quantity > 0) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  }, [quantity]);
+
+  const handleCommanderPress = useCallback(() => {
+    const selectedQuantity = quantity;
+    setQuantity(0);
+
+    navigation.navigate("Patients", {
+      screen: "Commande de la semaine",
+      params: {
+        element,
+        Quantity: selectedQuantity,
+      },
+    });
+  }, [element, quantity, navigation]);
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Button onPress={handleDecrement} title="-" />
+      <Text> </Text>
+      <Text style={{ textAlignVertical: "center" }}>{quantity}</Text>
+      <Text> </Text>
+      <StatusBar />
+      <Button onPress={handleIncrement} title="+" />
+      <Text> </Text>
+      <Button title="Commander" onPress={handleCommanderPress} />
+    </View>
+  );
+});
+
+export default Counter;
 
 
-export default class Counter extends React.Component {
-    state ={
-        value : 0
-    }
 
-    incrementValue = () => {
-        this.setState({
-            value : this.state.value + 1
-        })
-    }
 
-    decrementValue = () => {
-        if (this.state.value > 0){
-            this.setState({
-                value : this.state.value -1
-            })}
-    }
-    render(){
-        return(
-            <View style={{flexDirection:'row'}}>
-                <Button onPress={this.decrementValue} title={"-"} />
-                <Text>  </Text>
-                <Text style={{textAlignVertical:'center'}}>{this.state.value}</Text>
-                <Text>  </Text>
-                <StatusBar/>
-                <Button onPress={this.incrementValue} title={"+"} />
-            </View>
-        );
-    }
-}
